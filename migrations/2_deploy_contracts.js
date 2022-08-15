f4b00c60e69a842ab431965be098e61365f1f700
Steve <johnw@motif.foundation>
2022-08-15 18:56:39 +0300

    Initial commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,26 @@
+// const WrappedMotif = artifacts.require('WrappedMotif');
+// const MotifETH = artifacts.require('MotifETH');
+// const MotifUSD = artifacts.require('MotifUSD');
+ 
+// module.exports = async function (deployer, network, accounts) {
+
+//     await deployer.deploy(WrappedMotif); 
+//     const wrappedMotif = await WrappedMotif.deployed(); 
+//     console.log("WMOTIF Address", wrappedMotif.address)
+
+//     await deployer.deploy(MotifUSD); 
+//     const motifUSD = await MotifUSD.deployed(); 
+//     console.log("MotifUSD Address", motifUSD.address)
+
+//     await deployer.deploy(MotifETH); 
+//     const motifETH = await MotifETH.deployed(); 
+//     console.log("MotifETH Address", motifETH.address)
+
+//     //HelloWorld.deployed().then(function(instance){return instance.sayHello()});Also, Read
+
+
+
+//     console.log("all done")
+// }
+
+
